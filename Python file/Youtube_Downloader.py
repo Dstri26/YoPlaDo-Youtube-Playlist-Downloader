e74f3732e813d137688ff838d7f7b0b3b9e04bf9
Trideep Barik <trideep.india@gmail.com>
2021-06-29 17:10:34 +0530

    Update Youtube_Downloader.py

--- Python file/Youtube_Downloader.py
+++ Python file/Youtube_Downloader.py
@@ -7,40 +7,34 @@ Created on Sun Mar  8 20:12:07 2020
 """
 Example of WebScrapping using python
 """
+
 from selenium import webdriver
-import time 
-import youtube_dl 
+import time
+from pytube import YouTube as yt
 import os
+from selenium.webdriver.chrome.options import Options  
 
 ydl_opts = {} 
-
-def vidstrip(playlist):
-    for i in range(len(playlist)):
-        end=playlist[i].find("&")
-        playlist[i]=playlist[i][:end]
-    return playlist
     
 url = input("Enter youtube playlist link : ")
-driver = webdriver.Chrome() 
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
 time.sleep(5)
 playlist=[]
-videos=driver.find_elements_by_class_name('style-scope ytd-playlist-video-renderer')
+videos=driver.find_elements_by_id('video-title')
 for video in videos:
-    link=video.find_element_by_xpath('.//*[@id="content"]/a').get_attribute("href")
+    link=video.get_attribute("href")
+    end=link.find("&")
+    link=link[:end]
     playlist.append(link)
-    
-vidlist=vidstrip(playlist)
-count=1
 os.chdir('C:/Users/Trideep/Downloads') 
 
-for link in vidlist:
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([link])
-    except:
-        print("Exception occured. Either the video has no quality as set by you, or it is not available. Skipping video {number}".format(number = count))
-        continue
-    count += 1
-
+for link in playlist:
+    print(link)
+    vid=yt(link)
+    vid.streams.get_highest_resolution().download()
+    print(vid.title+" has been successfully downloaded !!")
 driver.close()
